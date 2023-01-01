8d57c2ffbee2d276bc8e4975a2b89224b8d16a55
zasabri <zasabri@gmail.com>
2023-01-01 02:54:01 +0100

    zasabri

--- sort_algorithm.c
+++ sort_algorithm.c
@@ -6,13 +6,13 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/01 00:50:40 by zasabri           #+#    #+#             */
-/*   Updated: 2023/01/01 02:14:38 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/01/01 02:53:35 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-int	*stack_a_creation(t_list *head)
+int	*stack_a_creation(t_list *head, int size)
 {
 	t_list	*save;
 	int		*arr;
@@ -20,11 +20,8 @@ int	*stack_a_creation(t_list *head)
 
 	save = head;
 	i = 0;
-	while (head)
-	{
-		head=head->next;
+	while (i < size - 1)
 		i++;
-	}
 	arr = malloc(i * 4);
 	i = 0;
 	while (save)
@@ -36,17 +33,14 @@ int	*stack_a_creation(t_list *head)
 	return (arr);
 }
 
-int	*stack_b_creation(t_list *head)
+int	*stack_b_creation(int size)
 {
 	int		*arr;
 	int		i;
 
 	i = 0;
-	while (head)
-	{
-		head=head->next;
+	while (i < size - 1)
 		i++;
-	}
 	arr = malloc(i * 4);
 	return (arr);
 }
@@ -56,6 +50,6 @@ void	sort_algorithm(t_list *head, int size)
 	int	*stack_a;
 	int	*stack_b;
 
-	stack_a = stack_a_creation(head);
-	stack_b = stack_b_creation(head);
+	stack_a = stack_a_creation(head, size);
+	stack_b = stack_b_creation(size);
 }
