1d1484f531644716bd18369ca4b3d2f2b485de97
zasabri <zasabri@gmail.com>
2023-01-30 10:58:59 +0100

    zasabri

--- sort_hundred.c
+++ sort_hundred.c
@@ -6,7 +6,7 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 02:24:25 by zasabri           #+#    #+#             */
-/*   Updated: 2023/01/30 04:26:41 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/01/30 10:58:32 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,46 +14,31 @@
 
 void	hundreds(t_list **head, t_list **head2, int *arr)
 {
-	int	mid;
-	int	offest;
-	int	start;
-	int	end;
 	int	i;
-	int size = ft_lstsize(*head);
+	int	range;
 	
-	mid	= ft_lstsize(*head) / 2 - 1;
-	offest = ft_lstsize(*head) / mid;
-	start = mid - offest;
-	end = mid + offest;
 	i = 0;
-	while (i < size)
+	if (ft_lstsize(*head) >= 6 && ft_lstsize(*head) <= 16)
+		range = 3;
+	else if (ft_lstsize(*head) <= 100)
+		range = 15;
+	else if (ft_lstsize(*head) <= 500)
+		range = 25;
+	while (*head)
 	{
-		if ((*head)->content >= arr[start] && (*head)->content <= arr[end])
+		if ((*head)->content == arr[i])
 		{
 			push_b_algo(head, head2);
-			if ((*head2)->content < arr[mid])
-				rotate_b_algo(head2);
-			i = 0;
+			rotate_b_algo(head2);
+			i++;
 		}
+		else if ((*head)->content >= arr[i] && (*head)->content <= arr[range])
+			push_b_algo(head, head2);
 		else
 		{
 			rotate_a_algo(head);
-			i++;
-		}
-		if (i == end)
-		{
-			start -= offest;
-			end += offest;
+			range++;
 		}
 	}
-	push_b_algo(head, head2);
-	if ((*head2)->content < (*head2)->next->content)
-		swap_b_algo(head2);
-	push_a_algo(head, head2);
-	// while (head2)
-	// {
-	// 	push_a_algo(head, head2);
-	// 	// if ((*head)->content > (*head)->next->content)
-	// 	// 	rotate_a_algo(head);
-	// }
+	
 }
