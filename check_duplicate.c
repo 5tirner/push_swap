7c1c3440e777efc55f8a964ff00b26f59e48458c
zasabri <zasabri@gmail.com>
2023-01-31 10:33:38 +0100

    zasabri

--- check_duplicate.c
+++ check_duplicate.c
@@ -6,30 +6,19 @@
 /*   By: zasabri <zasabri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 03:34:51 by zasabri           #+#    #+#             */
-/*   Updated: 2023/01/31 09:13:24 by zasabri          ###   ########.fr       */
+/*   Updated: 2023/01/31 10:21:08 by zasabri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	if_duplcated(t_list *head)
+void	norm_thing_for_duplicates(int *arr, int size)
 {
 	int	i;
-	int	*arr;
-	int	size;
 	int	j;
 
 	i = 0;
-	size = ft_lstsize(head);
-	arr = malloc(size * 4);
-	while (head)
-	{
-		arr[i] = head->content;
-		i++;
-		head = head->next;
-	}
-	i = -1;
-	while (++i < size)
+	while (i < size)
 	{
 		j = i + 1;
 		while (j < size)
@@ -41,6 +30,24 @@ void	if_duplcated(t_list *head)
 			}
 			j++;
 		}
+		i++;
 	}
+}
+void	if_duplcated(t_list *head)
+{
+	int	i;
+	int	*arr;
+	int	size;
+
+	i = 0;
+	size = ft_lstsize(head);
+	arr = malloc(size * 4);
+	while (head)
+	{
+		arr[i] = head->content;
+		i++;
+		head = head->next;
+	}
+	norm_thing_for_duplicates(arr, size);
 	free(arr);
 }
